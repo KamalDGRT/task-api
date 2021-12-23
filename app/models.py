647445cf227dc17846dd739fb81cf9f3021824bb
KamalDGRT <kamaldgrt@gmail.com>
2021-12-23 16:14:57 +0530

    alembic: create employee table

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 # Every model represents a table in our database.
 
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -17,3 +18,28 @@ class EmployeeType(Base):
         nullable=False,
         server_default=text('now()')
     )
+
+
+class Employee(Base):
+    __tablename__ = "employee"
+
+    employee_id = Column(Integer, primary_key=True, index=True)
+    employee_name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+
+    employee_type_id = Column(
+        Integer,
+        ForeignKey("employee_type.employee_type_id", ondelete="CASCADE"),
+        nullable=False
+    )
+    created_at = Column(TIMESTAMP(
+        timezone=True),
+        nullable=False,
+        server_default=text('now()')
+    )
+
+    # This is gonna create another property for us for our employee table
+    # so that when we retrieve our employee details, it will fetch the
+    # properties of the employee_type table
+    employee_type = relationship("EmployeeType")
